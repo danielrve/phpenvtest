ea73ed2ccf12719aa228fa483c7b3afd72da87ec
Daniel Rivas <34517888+danielrve@users.noreply.github.com>
2023-02-25 18:36:27 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,20 @@
 
 echo "Hello World!";
   
+echo "<BR>";
   
 $ip = getenv('REMOTE_ADDR');
 
 echo $ip;
+echo "<BR>";
+
+$son = getenv('LASTNAME');
+
+echo $son;
+echo "<BR>";
 
 $name = getenv("DBNAME");
 
 echo $name;
-
+echo "<BR>";
 ?>
